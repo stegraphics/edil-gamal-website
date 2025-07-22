b98273add4a48dee11ad4746cbd5b862779b1e23
Stefano <stefanoschifanographic@gmail.com>
2025-07-22 13:00:12 +0200

    Rimozione di vite-plugin-rewrite-all per incompatibilità con Vite 5

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import rewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), rewriteAll()],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
